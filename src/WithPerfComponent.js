6ce0ac858afeff0c22ac08974aaf91f76cd95cc8
Monika Dhok <modhok@microsoft.com>
2018-09-18 11:39:50 +0530

    removing settimeouts

--- src/WithPerfComponent.js
+++ src/WithPerfComponent.js
@@ -7,48 +7,60 @@ const WithPerfComponent = WrappedComponent => {
         constructor(props) {
             super(props);
             this.state = {
+                counter: true,
                 showComponent: true
             }
             this.addEventListeners = this.addEventListeners.bind(this);
-            this.mountedAgain = this.mountedAgain.bind(this);
             this.unmounted = this.unmounted.bind(this);
+            this.cleanStateAddListeners = this.cleanStateAddListeners.bind(this);
         }
 
         render() {
             return (
                 <div>
+                    <p> {this.state.counter} </p>
                     {this.state.showComponent && <WrappedComponent />}
                 </div>
             );
         }
 
+        cleanStateAddListeners() {
+            //Clean the previous components with multiple unmounts.
+            setTimeout(() => {
+                this.setState({ counter: 1 }, () => { });
+            }, 2000);
+            setTimeout(() => {
+                this.setState({ counter: 2 }, () => { });
+                console.log("Counter incremented again");
+                this.addEventListeners();
+                console.timeStamp("HOCMounted");
+            }, 5000);
+        }
+
         addEventListeners() {
-            //window.addEventListener('HOCMounted', () => {
-              //  this.setState({ showComponent: false }, this.unmounted);
-                this.setState({ showComponent: false }, () => {});
-           // })
+            window.addEventListener('HOCMounted', () => {
+                console.log("Captured event: HOCMounted!");
+                this.setState({ showComponent: false }, () => this.unmounted);
+            })
         }
 
         unmounted() {
-            console.log("HOCUnmounted!");
-            console.timeStamp("HOCUnmounted");
-            window.addEventListener('HOCUnmounted', () => {
-                this.setState({ showComponent: true }, this.mountedAgain);
-            });
-        }
-
-        mountedAgain() {
-            console.log("HOCMountedAgain");
-            console.timeStamp("HOCMountedAgain");
-            window.addEventListener('HOCMountedAgain', () => {
-                console.log("took all snapshots!")
-            })
+            console.log("after unmounted");
+            setTimeout(() => {
+                this.setState({ counter: 1 }, () => { });
+                console.log("Counter incremented");
+            }, 2000);
+            setTimeout(() => {
+                this.setState({ counter: 2 }, () => { });
+                console.log("Counter re-incremented");
+                console.log("HOCUnmounted!");
+                console.timeStamp("HOCUnmounted");
+            }, 6000);
         }
 
         componentDidMount() {
             console.log("HOC : mounted!");
-            console.timeStamp("HOCMounted");
-            setTimeout(() => this.addEventListeners(), 10000);
+            this.cleanStateAddListeners();
         }
     }
 }
